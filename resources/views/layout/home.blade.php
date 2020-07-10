3ccd7538fe8a01e03e2c07369ccb152cf3f0970f
surageat <surageat12345@gmail.com>
2020-07-10 16:27:10 +0700

    .

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Blank</title>
+  <title>Meet informatic system</title>
 
   <!-- Custom fonts for this template-->
   <link href="{{asset('all/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
@@ -41,6 +41,7 @@
 
       <!-- Divider -->
       <hr class="sidebar-divider">
+
 
       <!-- Heading -->
       <div class="sidebar-heading">
