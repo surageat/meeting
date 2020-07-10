7665ca1358e668f7ca9f9d45d849f351aeb7acd1
surageat <surageat12345@gmail.com>
2020-07-10 15:23:58 +0700

    .

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -353,7 +353,6 @@
   <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
   </a>
-testtt
   <!-- Logout Modal-->
   <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
