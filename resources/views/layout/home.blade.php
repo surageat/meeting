199b76aa4e8b6e5c787b1a11dead02c0261a3bdd
surageat <surageat12345@gmail.com>
2020-07-21 16:35:24 +0700

    .....

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -266,7 +266,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>informatic &copy; Meeting System</span>
+            <span>Information &copy; Meeting System</span>
           </div>
         </div>
       </footer>
